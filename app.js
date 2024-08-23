d59eda8630ae79e78c1d8c4bca6724a1c0dc6888
unknown <soulaymaneakc@gmail.com>
2024-08-23 15:56:32 +0200

    end

--- app.js
+++ app.js
@@ -6,15 +6,14 @@ async function searchMeals(){
     try {
         const resultApi = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${plat}`);
         const data = await resultApi.json();
-        // console.log(data);
 
         if(data.meals){
             data.meals.forEach(meal => {
                 const mealDiv = document.createElement("div");
-                mealDiv.classList.add('meal');
+                mealDiv.classList.add('meal-item');
 
                 const mealImg = document.createElement("img");
-                mealImg.src = meal.strMealThumb;
+                mealImg.src = meal.strMealThumb ? meal.strMealThumb : 'default-image.jpg';  // Utilisez une image par défaut si aucune n'est disponible
 
                 const mealInfo = document.createElement("div");
                 mealInfo.innerHTML = `<h2>${meal.strMeal}</h2><p>${meal.strInstructions}</p>`;
@@ -25,7 +24,7 @@ async function searchMeals(){
                 listPlat.appendChild(mealDiv);
             });
         } else {
-            listPlat.innerHTML = '<p>pas de plats trouvé.</p>';
+            listPlat.innerHTML = '<p>Pas de plats trouvés.</p>';
         }
     } catch (error){
         console.log('Erreur: ', error);
